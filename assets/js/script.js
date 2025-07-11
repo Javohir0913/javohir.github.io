c13847f5f4322e0b1759f4980d75855feaaf3e0d
Javohir0913 <pro100java01@gmail.com>
2025-07-11 17:52:45 +0500

    rest 20

--- assets/js/script.js
+++ assets/js/script.js
@@ -411,53 +411,6 @@ Version      : 1.0
 
     });
 
-
-
-    /* ==========================================================================
-       When document is loaded, do
-       ========================================================================== */
-       
-let preloaderStarted = false;
-
-function startPreloaderAnimation() {
-    if (preloaderStarted) return;
-    preloaderStarted = true;
-
-    const svg = document.getElementById("preloaderSvg");
-    const tl = gsap.timeline();
-    const curve = "M0 502S175 272 500 272s500 230 500 230V0H0Z";
-    const flat = "M0 2S175 1 500 1s500 1 500 1V0H0Z";
-
-    tl.to(".preloader-heading .load-text", {
-        delay: 0.5,
-        y: -100,
-        opacity: 0,
-    });
-    tl.to(svg, {
-        duration: 0.3,
-        attr: { d: curve },
-        ease: "power2.easeIn",
-    }).to(svg, {
-        duration: 0.3,
-        attr: { d: flat },
-        ease: "power2.easeOut",
-    });
-    tl.to(".preloader", {
-        y: -1500,
-    });
-    tl.to(".preloader", {
-        duration: 0.5,
-        zIndex: -1,
-        display: "none",
-    });
-}
-
-// ✅ 0.1 sekunddan keyin ishga tushadi (faqat 1 marta)
-setTimeout(() => {
-    startPreloaderAnimation();
-}, 100);
-
-
     /*
      * ----------------------------------------------------------------------------------------
      *  CUSTOM CURSOR JS
